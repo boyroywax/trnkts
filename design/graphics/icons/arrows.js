3861a1c6dfb6173e8457373f174e19fb1a5cc0e4
boyroywax <jmsdevln@gmail.com>
2025-07-16 23:38:52 -0700

    Refactor icon SVG paths for improved clarity and consistency across communication, interface, media, and system icons. Enhance index.html with a new icon library overview, including gradient previews and feature highlights.

--- design/graphics/icons/arrows.js
+++ design/graphics/icons/arrows.js
@@ -3,65 +3,65 @@ const arrowIcons = {
     'arrow-up': {
         name: 'Arrow Up',
         paths: [
-            'M18 15l-6-6-6 6',
-            'M12 19V5',
-            'M12 9l-2 2m4-2l-2 2'
+            'M18 15l-6-6-6 6M12 19V5',
+            'M16 13l-4-4-4 4M12 17V7',
+            'M14 11l-2-2-2 2M12 15V9'
         ]
     },
     'arrow-down': {
         name: 'Arrow Down',
         paths: [
-            'M6 9l6 6 6-6',
-            'M12 5v14',
-            'M12 15l-2-2m4 2l-2-2'
+            'M6 9l6 6 6-6M12 5v14',
+            'M8 11l4 4 4-4M12 7v10',
+            'M10 13l2 2 2-2M12 9v6'
         ]
     },
     'arrow-left': {
         name: 'Arrow Left',
         paths: [
-            'M15 18l-6-6 6-6',
-            'M19 12H5',
-            'M9 12l2-2m-2 2l2 2'
+            'M15 18l-6-6 6-6M19 12H5',
+            'M13 16l-4-4 4-4M17 12H7',
+            'M11 14l-2-2 2-2M15 12H9'
         ]
     },
     'arrow-right': {
         name: 'Arrow Right',
         paths: [
-            'M9 18l6-6-6-6',
-            'M5 12h14',
-            'M15 12l-2-2m2 2l-2 2'
+            'M9 18l6-6-6-6M5 12h14',
+            'M11 16l4-4-4-4M7 12h10',
+            'M13 14l2-2-2-2M9 12h6'
         ]
     },
     'chevron-up': {
         name: 'Chevron Up',
         paths: [
-            'M6 15l6-6 6 6',
-            'M8 13l4-4 4 4',
-            ''
+            'M18 15l-6-6-6 6',
+            'M16 13l-4-4-4 4',
+            'M14 11l-2-2-2 2'
         ]
     },
     'chevron-down': {
         name: 'Chevron Down',
         paths: [
-            'M18 9l-6 6-6-6',
-            'M16 11l-4 4-4-4',
-            ''
+            'M6 9l6 6 6-6',
+            'M8 11l4 4 4-4',
+            'M10 13l2 2 2-2'
         ]
     },
     'expand': {
         name: 'Expand',
         paths: [
-            'M15 3h6v6m-6 6h6v6M9 3H3v6m6 6H3v6',
-            'M21 3l-7 7m0 4l7 7M3 3l7 7m-7 4l7 7',
-            ''
+            'M15 3h6v6M9 21H3v-6M21 3l-7 7M3 21l7-7M15 21h6v-6M9 3H3v6M21 21l-7-7M3 3l7 7',
+            'M17 5h4v4M7 19H3v-4M19 5l-5 5M5 19l5-5M17 19h4v-4M7 5H3v4M19 19l-5-5M5 5l5 5',
+            'M18 6h2v2M6 18H4v-2M18 6l-3 3M6 18l3-3M18 18h2v-2M6 6H4v2M18 18l-3-3M6 6l3 3'
         ]
     },
     'compress': {
         name: 'Compress',
         paths: [
-            'M8 3v3h3m2 0V3h3M8 21v-3h3m2 0v3h3',
-            'M11 6h2v2m0 8h-2v2',
-            ''
+            'M8 3v3a2 2 0 01-2 2H3M21 8h-3a2 2 0 01-2-2V3M3 16h3a2 2 0 012 2v3M16 21v-3a2 2 0 012-2h3',
+            'M9 4v2a1 1 0 01-1 1H6M20 9h-2a1 1 0 01-1-1V6M4 15h2a1 1 0 011 1v2M15 20v-2a1 1 0 011-1h2',
+            'M10 5v1H9M19 10h-1v-1M5 14h1v1M14 19v-1h1M8 8l-2-2M16 8l2-2M8 16l-2 2M16 16l2 2'
         ]
     }
 };
