bc34a8023d9636a4a77353bc5edd43c643226379
boyroywax <jmsdevln@gmail.com>
2025-07-17 10:23:36 -0700

    -added web3 icons

---
+++ design/graphics/icons/gems.js
@@ -0,0 +1,104 @@
+// Gems Icons - Trnkts brand theme icons
+const gemsIcons = {
+    'trnkts-logo': {
+        name: 'Trnkts Logo',
+        paths: [
+            'M12 2l8 5-8 5-8-5 8-5zM4 7l8 5v10l-8-5V7zM20 7l-8 5v10l8-5V7z'
+        ]
+    },
+    'gem': {
+        name: 'Gem',
+        paths: [
+            'M6 3h12l4 6-10 12L2 9l4-6z'
+        ]
+    },
+    'diamond': {
+        name: 'Diamond',
+        paths: [
+            'M6 3h12l3 5-9 13L3 8l3-5zM6 8h12M9 3v5M15 3v5'
+        ]
+    },
+    'crystal': {
+        name: 'Crystal',
+        paths: [
+            'M12 2l6 4v8l-6 8-6-8V6l6-4zM6 6l6 6 6-6M6 14l6-6 6 6'
+        ]
+    },
+    'ruby': {
+        name: 'Ruby',
+        paths: [
+            'M5 5h14l2 4-9 12L3 9l2-4zM5 9h14M8 5l-1 4M16 5l1 4M12 5v4'
+        ]
+    },
+    'emerald': {
+        name: 'Emerald',
+        paths: [
+            'M8 3h8l4 4v8l-4 4H8l-4-4V7l4-4zM8 7h8M8 17h8M4 11h16'
+        ]
+    },
+    'sapphire': {
+        name: 'Sapphire',
+        paths: [
+            'M12 2l5 3v6l-5 11-5-11V5l5-3zM7 8h10M9 5l3 3 3-3'
+        ]
+    },
+    'prism': {
+        name: 'Prism',
+        paths: [
+            'M4 6h16l-2 12H6L4 6zM4 6l8 12M20 6l-8 12M6 18h12'
+        ]
+    },
+    'sparkle': {
+        name: 'Sparkle',
+        paths: [
+            'M12 1l3 6 6 3-6 3-3 6-3-6-6-3 6-3 3-6zM18 4l1 2 2 1-2 1-1 2-1-2-2-1 2-1 1-2zM6 16l1 2 2 1-2 1-1 2-1-2-2-1 2-1 1-2z'
+        ]
+    },
+    'treasure': {
+        name: 'Treasure',
+        paths: [
+            'M5 8h14a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2v-8a2 2 0 012-2zM8 8V6a1 1 0 011-1h6a1 1 0 011 1v2M12 12l3 3H9l3-3z'
+        ]
+    },
+    'crown': {
+        name: 'Crown',
+        paths: [
+            'M5 16h14l2-8-5 3-4-7-4 7-5-3 2 8zM5 16v2h14v-2'
+        ]
+    },
+    'jewel': {
+        name: 'Jewel',
+        paths: [
+            'M12 2l4 4H8l4-4zM8 6h8l4 4-8 12L4 10l4-4z'
+        ]
+    },
+    'shine': {
+        name: 'Shine',
+        paths: [
+            'M12 1v6M12 17v6M23 12h-6M7 12H1M20.5 3.5l-4.2 4.2M7.7 16.3l-4.2 4.2M20.5 20.5l-4.2-4.2M7.7 7.7L3.5 3.5'
+        ]
+    },
+    'brilliant': {
+        name: 'Brilliant',
+        paths: [
+            'M12 2l6 6-6 6-6-6 6-6zM12 8l6 6-6 6-6-6 6-6zM6 8l6 6M18 8l-6 6M6 14l6-6M18 14l-6-6'
+        ]
+    },
+    'gemstone': {
+        name: 'Gemstone',
+        paths: [
+            'M7 4h10l3 3v6l-8 9-8-9V7l3-3zM7 7h10M9 4v3M15 4v3M12 10v12'
+        ]
+    },
+    'precious': {
+        name: 'Precious',
+        paths: [
+            'M12 2l4 4 4-4v4l-4 4 4 4v4l-4-4-4 4v-4l4-4-4-4V2z'
+        ]
+    }
+};
+
+// Export for use in main icons file
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = gemsIcons;
+}
